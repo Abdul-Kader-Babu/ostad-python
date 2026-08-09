2a77084d6e2f8116860c1d050b5add12b0ffa6f7
Abdul-Kader-Babu <abdulkaderbabubd@gmail.com>
2026-08-10 00:05:47 +0600

    Add assignment solutions

---
+++ One.py
@@ -0,0 +1,19 @@
+# print("hello world");
+# print("20", "07", "23", sep="/");
+
+# print("hello world", end="");
+# print("how are you");
+
+# print("hello world", end="\n");
+# print("How Are You");
+
+# print(f"total:{10+10+10}");
+# print("total:{10+10+10}");
+
+# print(f"Total With Separator:{10+1000+10:,}");
+
+print(f"left aligned: {10000:^10}.");
+
+
+
+
---
+++ classTwo.py
@@ -0,0 +1,123 @@
+# print
+# print("abdul Kader babu");
+# print("Bangladesh");
+# print(100+500);
+# print("I Love Python");
+# print("My Age Is", 24);
+
+# #sep
+# print("java", "python", "javasript", sep="-");
+# print("A", "B", "C", sep="*");
+# print("A", "B", "C", sep="______");
+# print("Dhaka", "Sylhet", "Khulna", "Barishal", sep=",");
+# print(10,20,30, sep="+");
+# print("A","B","C", sep=" ");
+# print("Java", "Python", "C++", sep="\n");
+# print("WWW", "Google", "Com", sep=".");
+# #Homework
+# print("Python", "Java", "C++", sep="|");
+# print(10,20,30,40,50, sep="-");
+# print("A","B","C","D",sep="=");
+# print("HTML", "CSS", "JAVASCRIPT", sep=".");
+# print("Bangladesh", "India", "Nepal", sep="->");
+
+# #End
+# print("Python", end=" ");
+# print("Java", end=" ");
+# print("C++", end=" ");
+# print("\n");
+# print(10, end="-");
+# print(20, end="-");
+# print(30);
+# print("Username", end=":");
+# print("Babu");
+# print("Loading", sep=".......");
+# print("Done");
+# #Homework
+# print("Python", end="->");
+# print("Java", end="->");
+# print("C++");
+# print("I", "Love", "Python", sep=" ");
+
+# # f(formatting)
+# name = "Babu";
+# print(f"Hello {name}");
+# age = 24;
+# print(f"Im {age} Years Old");
+# print(f"50+70 = {50+70}");
+
+# name = "Babu";
+# age = 24;
+# country = "Bangladesh";
+# print(f"Name: {name}");
+# print(f"Age: {age}");
+# print(f"Country: {country}");
+
+# #input
+# name = input("Enter Your Name:");
+# age = int(input("Enter Your Age: "));
+# country = input("Enter Your Country :");
+# language = input("Enter Your Favourite Language: ")
+# # print(f"Your Name is {name}, you are {age} years old, you are from {country} and your language is {language} right?");
+# print(type(age));
+
+
+# type convrsion
+# number = input("Enter Your Number: ");
+# print(f"your Number is {number} and it is a ", type(number));
+# conversiontoInt = int(number);
+# print(f"now Your number is {type(conversiontoInt)}");
+# number = input("Enter YOur Number: ");
+# print(f"Your Number is {number}");
+# numberToFloat = float(number);
+# print(numberToFloat);
+
+# string = input("Enter Your Number: ");
+# print(f"Your Number is {string} and it is a{type(string)}",);
+# numberToString = int(string);
+# print(f"NoW it is a {type(numberToString)}");
+
+# # print(int("50"));
+# print(float(25));
+
+# print(type(str(100)));
+# userAge = int(input("Enter Your Age: "));
+# afterYears = int(input("Enter The Year: "));
+# print(f"Your Age is {userAge} and after {afterYears} years you will {userAge+afterYears}");
+
+# height = float(input("Enter Your hegiht: "));
+# print(f"Your Height is {height}");
+
+# x = int(input("Enter The First Number: "));
+
+# y = int (input("Enter The Second Number: "));
+# print(f"Sum Of Your Numbers Are {x+y}");
+
+# price = float(input("Enter The Price: "));
+# print(f"The Price is {price}");
+
+# a = int(input("Enter The First Number: "));
+# b = int(input("Enter The second Number: "));
+# c = int(input("Enter The third Number: "));
+# average = (a+b+c)/3;
+# print(f"Average is {int(average)}");
+
+
+# format specifier
+# number = 1222522.201444;
+# print(f"{number:.3f}");
+
+# name = "Babu";
+# print(f"|{name:^10}|");
+
+# print("Orange",  "Mango", "Apple", sep=",");
+# print("Orange",  "Mango", "Apple", sep="    ");
+
+# print("12", "03", "2026", sep="/");
+# name = "Abdul Kader Babu";
+# age = 24;
+# country = "Bangladesh";
+# print(name, age, country, sep="  | ");
+
+
+print("*","*","*","*","*", sep="");
