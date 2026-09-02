a2efad55e51bfe0029fb5e6920aa144963324353
Abdul-Kader-Babu <147910116+Abdul-Kader-Babu@users.noreply.github.com>
2026-09-02 21:34:18 +0600

    Add files via upload

---
+++ m3assignment.py
@@ -0,0 +1,42 @@
+
+#problem 1:
+# for number in range(1,11):
+#     print(number)
+
+
+# problem - 2
+# n = int(input("Enter A Number: "))
+# i = 1
+# sum =0
+# while i<=n:
+#     sum += i
+    
+#     print(f"{i}. {sum}")
+#     i +=1
+
+# problem - 3
+
+# numbers = int(input("Enter A positive Number: "));
+
+
+# if numbers>=1:
+#     even = numbers//2
+    
+#     print(f"Total Even Number between {1} to {numbers} is {even}")
+# else: print("Please Enter A Positive Number")
+
+# problem - 4
+
+# number = int(input("Enter A Number Between 1 to 10: "))
+
+# if number>10 or number<1:
+#     print("Please Enter A Number Between 1 to 10")
+# else: 
+#     for numbers in range(1,11):
+#         multiple = number*numbers
+#         print(f"{number}*{numbers} = {multiple}")
+
+
+
+
+
