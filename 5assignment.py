85644350b620182a2df6130dbbe6f604e6280a4b
Abdul-Kader-Babu <147910116+Abdul-Kader-Babu@users.noreply.github.com>
2026-09-05 22:10:14 +0600

    Add files via upload

---
+++ 5assignment.py
@@ -0,0 +1,74 @@
+#part-1
+# marks = int(input("Enter Your Marks: "))
+
+# if marks>100 or marks<0:
+#     print("Invalid Marks")
+# elif marks>79:
+#     print("A+")
+# elif marks>70:
+#     print("A")
+# elif marks>60:
+#     print("B")
+# elif marks>50:
+#     print("C")
+# elif marks>=40:
+#     print("D")
+# else:
+#     print("Fail")
+
+#part-2
+
+# age = int(input("Enter Your Age: "))
+# if age<=0:
+#     print("Invalid Age")
+# elif age<8:
+#     print("A Child")
+# elif age<19:
+#     print("A Teenager")
+# elif age<31:
+#     print("An Adult")
+# else:
+#     print("A Senior Citizen")
+
+#part-3
+
+# counter = 1
+# while counter<=10:
+#     print(counter)
+#     counter+=1
+
+#part-4
+# import random
+
+
+# counter = 1
+# while True:
+#     number = random.randint(1,5)
+#     user = int(input("Enter The Correct Number: "))
+#     if number==user:
+#         print("Correct Answer")
+#         break
+#     elif number!=user:
+#         if number/2>=user:
+#             print("Too low")
+#         elif number<=user/2:
+#             print("Too High")
+#         else:
+#             print("Incorrect")
+#     print(f"correct number was {number}")
+#     counter+=1
+# print (f"you Try {counter} time")
+
+#part-5
+# for i in range (1,6):
+#     for j in range (i):
+#         print("*", end="")
+#     print()
+
+        
+
+
+
+
+
+
